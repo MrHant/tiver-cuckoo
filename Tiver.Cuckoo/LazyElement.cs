8e4f5304c5c33c811f31b859069bae7a5e6967ff
Artyom Silivonchik <mr.hant@gmail.com>
2017-12-12 09:24:52 +0300

    LazyWebDriver and LazyElement implementation

---
+++ Tiver.Cuckoo/LazyElement.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using Tiver.Fowl.Waiting;
+
+namespace Tiver.Cuckoo
+{
+    public class LazyElement : IWebElement, IWrapsElement
+    {
+        public LazyElement(IWebDriver driver, By locator)
+        {
+            _driver = driver;
+            _locator = locator;
+        }
+
+        // Process
+
+        public TResult Process<TResult>(Func<IWebElement, TResult> function)
+        {
+            var result = default(TResult);
+            Wait.Until(() =>
+            {
+                result = function.Invoke(WebElement);
+                return true;
+            }, typeof(NoSuchElementException), typeof(StaleElementReferenceException));
+
+            return result;
+        }
+
+        public void Process(Action<IWebElement> action)
+        {
+            Process(e =>
+            {
+                action.Invoke(e);
+                return true;
+            });
+        }
+
+        // Disable find
+
+        public IWebElement FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Lazy actions
+
+        public void Clear()
+        {
+            Process(e => e.Clear());
+        }
+
+        public void SendKeys(string text)
+        {
+            Process(e => e.SendKeys(text));
+        }
+
+        public void Submit()
+        {
+            Process(e => e.Submit());
+        }
+
+        public void Click()
+        {
+            Process(e => e.Click());
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            return Process(e => e.GetAttribute(attributeName));
+        }
+
+        public string GetProperty(string propertyName)
+        {
+            return Process(e => e.GetProperty(propertyName));
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            return Process(e => e.GetCssValue(propertyName));
+        }
+
+        public string TagName => Process(e => e.TagName);
+        public string Text => Process(e => e.Text);
+        public bool Enabled => Process(e => e.Enabled);
+        public bool Selected => Process(e => e.Selected);
+        public Point Location => Process(e => e.Location);
+        public Size Size => Process(e => e.Size);
+        public bool Displayed => Process(e => e.Displayed);
+        public IWebElement WrappedElement => WebElement;
+
+        private IWebElement WebElement => _driver.FindElement(_locator);
+        private readonly IWebDriver _driver;
+        private readonly By _locator;
+    }
+}
---
+++ Tiver.Cuckoo/LazyWebDriver.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+
+namespace Tiver.Cuckoo
+{
+    public class LazyWebDriver : IWebDriver, IJavaScriptExecutor, ITakesScreenshot, IWrapsDriver
+    {
+        public LazyWebDriver(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        // Lazy find
+
+        public IWebElement FindElement(By by)
+        {
+            return new LazyElement(WrappedDriver, by);
+        }
+
+        // Disable find elements
+
+        public ReadOnlyCollection<IWebElement> FindElements(By @by)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        // Not modified
+
+        public void Dispose()
+        {
+            _driver.Dispose();
+        }
+
+        public void Close()
+        {
+            _driver.Close();
+        }
+
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+
+        public IOptions Manage()
+        {
+            return _driver.Manage();
+        }
+
+        public INavigation Navigate()
+        {
+            return _driver.Navigate();
+        }
+
+        public ITargetLocator SwitchTo()
+        {
+            return _driver.SwitchTo();
+        }
+
+        public object ExecuteScript(string script, params object[] args)
+        {
+            return (_driver as IJavaScriptExecutor)?.ExecuteScript(script, args);
+        }
+
+        public object ExecuteAsyncScript(string script, params object[] args)
+        {
+            return (_driver as IJavaScriptExecutor)?.ExecuteAsyncScript(script, args);
+        }
+
+        public Screenshot GetScreenshot()
+        {
+            return (_driver as ITakesScreenshot)?.GetScreenshot();
+        }
+
+        public string Url
+        {
+            get { return _driver.Url; }
+            set { _driver.Url = value; }
+        }
+
+        public IWebDriver WrappedDriver => _driver;
+        public string Title => _driver.Title;
+        public string PageSource => _driver.PageSource;
+        public string CurrentWindowHandle => _driver.CurrentWindowHandle;
+        public ReadOnlyCollection<string> WindowHandles => _driver.WindowHandles;
+
+        private readonly IWebDriver _driver;
+    }
+}
